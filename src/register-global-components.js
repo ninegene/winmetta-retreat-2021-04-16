20fc2e605bc6f3f72f0e22556afbb4ce1f6167c8
aung <aung@a2mbp.local>
2021-04-15 00:18:54 -0700

    implment changes

--- src/register-global-components.js
+++ src/register-global-components.js
@@ -1,11 +1,13 @@
 import { register } from 'riot'
 import MyComponent from './components/global/my-component/my-component.riot'
 import Sidebar from './components/global/sidebar/sidebar.riot'
+import ReminderAudio from './components/global/reminder-audio/reminder-audio.riot'
 
 // Rollup can not generate this object automatically via require.context :(
 const componentsRegistry = {
   'my-component': MyComponent,
-  'sidebar': Sidebar
+  'sidebar': Sidebar,
+  'reminder-audio': ReminderAudio,
 }
 
 export default () => {
