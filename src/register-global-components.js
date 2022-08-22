3e6c503b829bacc5e6564a13f62f26f5b61094a6
aung <aung.oo@365smartshop.com>
2022-08-21 18:32:39 -0700

    feat: add another sc

--- src/register-global-components.js
+++ src/register-global-components.js
@@ -1,13 +1,15 @@
 import { register } from 'riot'
-import MyComponent from './components/global/my-component/my-component.riot'
 import Sidebar from './components/global/sidebar/sidebar.riot'
 import ReminderAudio from './components/global/reminder-audio/reminder-audio.riot'
+import ReminderAudio2 from './components/global/reminder-audio-2/reminder-audio-2.riot'
+import RetreatLinksHeader from './components/includes/retreat-links-header/retreat-links-header.riot'
 
 // Rollup can not generate this object automatically via require.context :(
 const componentsRegistry = {
-  'my-component': MyComponent,
-  sidebar: Sidebar,
-  'reminder-audio': ReminderAudio
+  'retreat-links-header': RetreatLinksHeader,
+  'reminder-audio': ReminderAudio,
+  'reminder-audio-2': ReminderAudio2,
+  sidebar: Sidebar
 }
 
 export default () => {
